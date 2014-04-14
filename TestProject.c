a3542283b667633bde0f55dcf620b077fcb5d190
bingshiue <bingshiue@gmail.com>
2014-04-15 01:44:26 +0800

    For Test Purpose
    Signed-off-by: bingshiue <bingshiue@gmail.com>

---
+++ TestProject.c
@@ -0,0 +1,23 @@
+/*
+ * TestProject.c
+ *
+ *  Created on: 2014/04/15
+ *      Author: bingshiue
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int main(int argc,char** argv){
+	int idx;
+
+	for(idx=0; idx<10; idx++)
+		fprintf(stdout,"New Train Project : %d \n",idx);
+
+	return 0;
+}
+
+
+
+
