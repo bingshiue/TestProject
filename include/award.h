ff9cb5159aed4a9b8ab2132e65e6fe6556c34254
bingshiue <bingshiue@gmail.com>
2014-04-20 16:48:15 +0800

    Add Award Define
    Match Award Item Define
    Straight Award Item Define
    
    Signed-off-by: bingshiue <bingshiue@gmail.com>

---
+++ include/award.h
@@ -0,0 +1,40 @@
+/*
+ * award.h
+ *
+ *  Created on: 2014/04/20
+ *      Author: bingshiue
+ */
+
+#ifndef AWARD_H_
+#define AWARD_H_
+
+
+enum MATCH_AWARD {
+	match_award_cherry  = 0, /**< Match Award Cherry */
+	match_award_orange,      /**< Match Award Orange */
+	match_award_apple,       /**< Match Award Apple */
+	match_award_coin,        /**< Match Award Coin */
+	match_award_bar,         /**< Match Award Bar */
+	match_award_diamond,     /**< Match Award Diamond */
+	match_award_crown,       /**< Match Award Crown */
+	match_award_freecoin,    /**< Match Award Freecoin */
+	match_award_roulette,    /**< Match Award Roulette */
+
+};/**< Match Award */
+
+enum STRAIGHT_AWARD {
+	straight_award_cherry = 0,/**< Straight Award Cherry */
+	straight_award_orange,    /**< Straight Award Orange */
+	straight_award_apple,     /**< Straight Award Apple */
+	straight_award_coin,      /**< Straight Award Coin */
+	straight_award_bar,       /**< Straight Award Bar */
+	straight_award_diamond,   /**< Straight Award Diamond */
+	straight_award_crown,     /**< Straight Award Crown */
+	straight_award_freecoin,  /**< Straight Award Freecoin */
+	straight_award_roulette,  /**< Straight Award Roulette */
+
+
+};/**< Straight Award */
+
+
+#endif /* AWARD_H_ */
